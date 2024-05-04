12f2968e37d24796b91f6d93ad377421871dee5c
Padrino43 <mkubis42@gmail.com>
2024-05-04 21:33:40 +0200

    init

---
+++ Kolokwium1/Kolokwium1/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿using Kolokwium1.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kolokwium1.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class Controller : ControllerBase
+{
+    private readonly IxRepository _xRepository;
+
+    public Controller(IxRepository xRepository)
+    {
+        _xRepository = xRepository;
+    }
+
+    // [HttpDelete("{id:int}")]
+    // public async Task<IActionResult> Remove(int id)
+    // {
+    //     var result = await db.RemoveStudentByIdAsync(id);
+    //     if (result) return NoContent();
+    //     return NotFound($"Student with id:{id} does not exist");
+    // }
+}
---
+++ Kolokwium1/Kolokwium1/Repositories/xRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kolokwium1.Repositories;
+
+public interface IxRepository
+{
+    
+}
+
+public class xRepository : IxRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public xRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<ActionResult> Getx(int id)
+    {
+        await using var sqlConnection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        var sqlCommand = new SqlCommand("SELECT "
+            , sqlConnection);
+        sqlCommand.Parameters.AddWithValue("@Id", id);
+        await sqlConnection.OpenAsync();
+        var reader = await sqlCommand.ExecuteReaderAsync();
+        await reader.ReadAsync();
+        return null;
+    }
+    
+    public async Task<int?> CreateProduct(int idWarehouse, int idProduct, int idOrder, double price, int amount, DateTime createdAt)
+    {
+        await using var sqlConnection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        var sqlCommand = new SqlCommand("UPDATE \"Order\" SET FulfilledAt = @FulfilledAt WHERE IdOrder = @IdOrder"
+            , sqlConnection);
+
+        await sqlConnection.OpenAsync();
+        var transaction = sqlConnection.BeginTransaction();
+        try
+        {
+            sqlCommand.Parameters.AddWithValue("@FulfilledAt", DateTime.Now);
+            sqlCommand.Parameters.AddWithValue("@IdOrder", idOrder);
+            sqlCommand.Transaction = transaction;
+            await sqlCommand.ExecuteNonQueryAsync();
+
+            sqlCommand.CommandText =
+                @"INSERT INTO Product_Warehouse (IdWarehouse, IdProduct, IdOrder, CreatedAt, Amount, Price)
+               VALUES (@IdWarehouse, @IdProduct, @IdOrder, @CreatedAt, @Amount, @Price);SELECT SCOPE_IDENTITY();";
+            sqlCommand.Parameters.Clear();
+            sqlCommand.Parameters.AddWithValue("@IdWarehouse", idWarehouse);
+            sqlCommand.Parameters.AddWithValue("@IdProduct", idProduct);
+            sqlCommand.Parameters.AddWithValue("@IdOrder", idOrder);
+            sqlCommand.Parameters.AddWithValue("@CreatedAt", createdAt);
+            sqlCommand.Parameters.AddWithValue("@Amount", amount);
+            sqlCommand.Parameters.AddWithValue("@Price", price * amount);
+            var idProductWarehouse =  Convert.ToInt32(await sqlCommand.ExecuteScalarAsync());
+
+            await transaction.CommitAsync();
+            return idProductWarehouse;
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync();
+            return null;
+        }
+    }
+}
